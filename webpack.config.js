b428c5f685562cf7bad91daf12409cd38140661c
Xiaoquan006Zhao <1009642031@qq.com>
2023-03-15 13:57:09 -0500

    adding babel

--- webpack.config.js
+++ webpack.config.js
@@ -14,7 +14,27 @@ module.exports = {
         test: /\.css$/,
         use: ["style-loader", "css-loader"],
       },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
     ],
+  },
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, "dist"),
+    },
+    port: 3000,
+    open: true,
+    hot: true,
+    compress: true,
+    historyApiFallback: true,
   },
   plugins: [
     new htmlWebpackPlugin({
